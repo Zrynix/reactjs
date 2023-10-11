6237276a0b4410b47bc725af23009ed07cb8b08a
Amine <aouraghamine406@gmail.com>
2023-10-11 15:30:24 +0200

    Messages

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import ExitSvg from "./ExitSvg";
 import Logo from "./logo";
 
 const Chatbox = ({ isOpen, setOpen }) => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(message === "") return;
+    setMessages(currentMessages => [...currentMessages,message]);
+    setMessage("");
+  }
   return (
     <div className={`chatbox ${isOpen ? "open" : "closed"}`}>
       <div className="frame">
@@ -13,25 +23,18 @@ const Chatbox = ({ isOpen, setOpen }) => {
         <div className="messages">
           <div className="message out">Hallo</div>
           <div className="message in">Hallo</div>'
-          <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div> <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div> <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div> <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div> <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div> <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div> <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div> <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div> <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div> <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div> <div className="message out">Hallo</div>
-          <div className="message in">Hallo</div>'
+          {messages.map((message) => (
+            <div className="message out">{message}</div>
+          ))}
         </div>
-        <div className="footer">
-          <input className="input" />
-          <button>
-            >
-          </button>
-        </div>
+        <form onSubmit={handleSubmit} className="footer">
+          <input
+            className="input"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button>{">"}</button>
+        </form>
       </div>
     </div>
   );
